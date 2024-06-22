a45a1e87e740111508b23675a723aee337d1ccb4
Ali Hasan <capt.3lii@gmail.com>
2024-06-22 15:56:56 +0300

    CM updates

--- Problems/removeDuplicate.js
+++ Problems/removeDuplicate.js
@@ -1,21 +1,20 @@
-// remove Duplicates number from a sorted array
-const removeDuplicates = function(nums) {
+function removeDuplicates(nums) {
     if (nums.length === 0) return 0;
 
-    let k = 1; // Initialize k to 1 since the first element is always unique
-
+    // Start from the second position
+    let k = 1; 
     for (let i = 1; i < nums.length; i++) {
         if (nums[i] !== nums[i - 1]) {
-            nums[k] = nums[i]; // Place the unique element at the k-th position
-            k++; // Increment k for the next unique element
+            nums[k] = nums[i]; 
+            k++; 
         }
     }
 
     return k;
-};
+}
 
 // Example usage:
-const nums = [-1,0,1,2,3,3,4,5];
+const nums = [0,0,1,1,1,2,2,3,3,4];
 const k = removeDuplicates(nums);
 console.log(k); // Output: 5
 console.log(nums.slice(0, k)); 
