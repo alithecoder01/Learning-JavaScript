f3125a2dbbd436ae334cffc2aa84716655b82398
Ali Hasan <capt.3lii@gmail.com>
2024-07-13 00:04:44 +0300

    Added constraints to the value entered

--- Problems/substn.js
+++ Problems/substn.js
@@ -1,19 +1,30 @@
-/**
- * @param {string} s
- * @return {number}
- */
-const s = "aliz"
+const s = "pwwkew";
 var lengthOfLongestSubstring = function (s) {
+  // Check the length constraint
+  if (s.length < 0 || s.length > 5 * 10 ** 4) {
+    throw new Error("The length of the string is out of bounds.");
+  }
+
+  // Check the character constraints
+  const validCharacters = new Set(
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  );
+  for (let char of s) {
+    if (!validCharacters.has(char)) {
+      throw new Error(`Invalid character found in the string: ${char}`);
+    }
+  }
+
   let charIndexMap = {};
   let left = 0;
   let maxLength = 0;
 
   for (let right = 0; right < s.length; right++) {
-      if (charIndexMap[s[right]] !== undefined) {
-          left = Math.max(left, charIndexMap[s[right]] + 1);
-      }
-      charIndexMap[s[right]] = right;
-      maxLength = Math.max(maxLength, right - left + 1);
+    if (charIndexMap[s[right]] !== undefined) {
+      left = Math.max(left, charIndexMap[s[right]] + 1);
+    }
+    charIndexMap[s[right]] = right;
+    maxLength = Math.max(maxLength, right - left + 1);
   }
 
   return maxLength;
